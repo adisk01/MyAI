44def6faa38c6b05615be0f39899c76ccf28e1ac
adisk01 <adiscrazyO1@gmail.com>
2023-05-27 19:35:47 +0530

    backend started

--- pages/api/room/index.js
+++ pages/api/room/index.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import connect from "../../database/connection"
+import connect from "../../../database/connection"
 export default function handler(req, res) {
+    con(400).json({error : "Database connection error"}))
   res.status(200).json({ name: 'John Doe' })
 }
